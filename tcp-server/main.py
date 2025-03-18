3123e8b6dd9159b382b139b2d20dda319a6cc73a
ahmed amer <ahmed.amer@arabiansystems.net>
2025-03-18 14:11:32 +0200

    echo server

--- tcp-server/main.py
+++ tcp-server/main.py
@@ -3,36 +3,47 @@ import socket
 
 class TCPServer:
     def __init__(self, host="127.0.0.1", port=5000):
-        self.host = host
-        self.port = port
+        self.addr = (host, port)
+        self.buffer_size = 8
 
     def start(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((self.host, self.port))
+            s.bind(self.addr)
             s.listen(1)
-            print(f"Server running on {self.host}:{self.port}")
+            print(f"Server running on {self.addr[0]}:{self.addr[1]}")
 
             while True:
                 conn, addr = s.accept()
                 print(f"Connection from {addr}")
 
-                data = conn.recv(1024 * 2)
-                print(data.decode())
+                data = conn.recv(1024 * self.buffer_size)
                 if not data:
                     break
 
-                # fmt: off
-                res = (
-                    'HTTP/1.1 200 OK\r\n'
-                    'Content-Type: application/json\r\n'
-                    '\r\n'
-                    '{"TEST": "TEST"}'
-                )
-                # fmt: on
+                res = self.handle_request(data.decode())
 
                 print(res)
+
                 conn.sendall(res.encode())
                 conn.close()
+
+    def handle_request(self, data: str) -> str:
+        status_line = "HTTP/1.1 200 OK\r\n"
+
+        headers_arr = [
+            "Content-Type: text",
+        ]
+
+        headers = ""
+        for header in headers_arr:
+            headers += header + "\r\n"
+
+        headers_end = "\r\n"
+
+        body = "Hello from the server \n" + data
+
+        res = f"{status_line}{headers}{headers_end}{body}"
+        return res
 
 
 if __name__ == "__main__":
